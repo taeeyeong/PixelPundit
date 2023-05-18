bac0928dc63e9cdef18628fbe76a3b355df0b3bc
taeeyeong <tyjung1024@gmail.com>
2023-05-18 15:55:25 +0900

    feat openai api

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-import os
-from llama_index import (
-    download_loader, 
-    SimpleDirectoryReader, 
-    LLMPredictor, 
-    GPTVectorStoreIndex,
-    GPTKeywordTableIndex, 
-    PromptHelper, 
-    ServiceContext)
 from langchain import OpenAI
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+from llama_index import (
+    download_loader,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    GPTVectorStoreIndex,
+    GPTKeywordTableIndex,
+    PromptHelper,
+    ServiceContext)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 
 SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
@@ -17,14 +20,16 @@ documents = loader.load_data()
 
 
 # define LLM
-llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-embedding-ada-002-v2"))
+llm_predictor = LLMPredictor(llm=OpenAI(
+    temperature=0, model_name="gpt-3.5-turbo"))
 
-max_input_size = 2048
+max_input_size = 4095
 num_output = 256
 max_chunk_overlap = 20
 prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
 
-service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+service_context = ServiceContext.from_defaults(
+    llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
 index = GPTKeywordTableIndex.from_documents(
     documents, service_context=service_context
@@ -33,4 +38,3 @@ index = GPTKeywordTableIndex.from_documents(
 query_engine = index.as_query_engine()
 response = query_engine.query("How to estimate pose parameters?")
 print(response)
-
